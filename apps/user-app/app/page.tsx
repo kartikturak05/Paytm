5123942bcb7e9e053f29ed57b56f81d3576b414c
Kartik Turak <kartikturak1@gmail.com>
2024-06-14 13:09:30 +0530

    Added recoil in app

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,17 @@
+"use client"
+
+// import React from 'react';
 import { PrismaClient } from "@repo/db/client";
+import {useBalance} from "@repo/store/useBalance"
 
 const client = new PrismaClient();
   
 
 export default function Home() {
+  const balance = useBalance();
   return (
     <div className="text-2xl font-bold">
-      hello 
+      hello  
     </div>
   );
 }
