f4d65dd1306ed0d100de5a33695198c638e0103b
Kartik Turak <kartikturak1@gmail.com>
2024-06-14 21:00:45 +0530

    Added add  money feature

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -18,10 +18,14 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <Providers>
-        <AppbarClient/>
-        <body className={inter.className}>{children}</body>
-      </Providers>
-    </html>
+    <Providers>
+      <body className={inter.className}>
+        <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
+          <AppbarClient />
+          {children}
+        </div>
+      </body>
+    </Providers>
+  </html>
   );
 }
